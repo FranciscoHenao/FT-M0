f7c8744fb0cf02054f6bf3516aa5374eecd3249e
FH <francisco_henao@hotmail.com>
2021-12-07 11:17:48 -0500

    x

---
+++ 05-JS-IV/homework/zfunction.js
@@ -0,0 +1,9 @@
+function saludar(par){
+    return console.log('meow'+' '+par);
+    }
+
+var n='YO';
+console.log(n);
+
+var x=saludar(n);
+console.log(x);
---
+++ 05-JS-IV/homework/ztoni.js
@@ -0,0 +1,26 @@
+var toni = 
+    {
+    nombre: 'Toni'
+}
+    
+toni.edad=33; 
+
+// saludar: function() 
+                // {
+                // console.log('hola, Toni');
+                // },
+    // hobbies: ['escalar', 'foto','teach'],
+    // }
+
+toni.saludar=function (){
+        console.log('meow');
+        }
+
+    console.log(toni);
+    console.log(toni.nombre);
+    // console.log(toni.saludar);
+    console.log(toni.edad);
+    console.log(toni.saludar);
+
+
+
